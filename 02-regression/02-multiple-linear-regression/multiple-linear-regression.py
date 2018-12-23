4594f0fe09551f99b3e8f27ab4bcc2c4793e94af
Minwoo Kim <mwkim0919@gmail.com>
2018-12-23 13:20:41 -0800

    working on Backward Elimination

--- 02-regression/02-multiple-linear-regression/multiple-linear-regression.py
+++ 02-regression/02-multiple-linear-regression/multiple-linear-regression.py
@@ -49,3 +49,15 @@ regressor.fit(X_train, y_train)
 
 # Predicting the Test Set Results
 y_pred = regressor.predict(X_test)
+
+# Building the optimal model for Backward Elimination
+import statsmodels.formula.api as sm
+X = np.append(arr = np.ones((50, 1)).astype(int), values = X, axis = 1)
+X_opt = X[:, [0, 1, 2, 3, 4, 5]]
+regressor_OLS = sm.OLS(endog = Y, exog = X_opt).fit()
+regressor_OLS.summary()
+X_opt = X[:, [0, 1, 3, 4, 5]]
+regressor_OLS = sm.OLS(endog = Y, exog = X_opt).fit()
+regressor_OLS.summary()
+
+# HW: finish Backward Ellimination
